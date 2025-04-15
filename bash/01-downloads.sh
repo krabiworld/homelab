5350873fc790599f98505d32ed1c1fde73c2e6ef
krabiworld <krabiworld@proton.me>
2025-04-16 00:14:41 +0300

    Add simple bash kubernetes deployment

---
+++ bash/01-downloads.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+wget -q --show-progress \
+  --https-only \
+  --timestamping \
+  -P downloads \
+  -i downloads.txt
+
+mkdir -p \
+  /etc/cni/net.d \
+  /opt/cni/bin \
+  /etc/containerd \
+  /etc/etcd \
+  /var/lib/etcd \
+  /var/lib/kubelet \
+  /var/lib/kube-proxy \
+  /var/lib/kubernetes \
+  /var/run/kubernetes
+
+cd downloads
+
+# CNI
+mkdir cni
+tar -xvf cni-plugins-linux-arm64-v1.6.2.tgz -C cni
+cp cni/* /opt/cni/bin
+cp configs/10-bridge.conf configs/99-loopback.conf /etc/cni/net.d/
+
+modprobe br-netfilter
+echo "br-netfilter" >> /etc/modules-load.d/modules.conf
+
+echo "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.d/kubernetes.conf
+echo "net.bridge.bridge-nf-call-ip6tables = 1" >> /etc/sysctl.d/kubernetes.conf
+sysctl -p /etc/sysctl.d/kubernetes.conf
+
+# runc
+install -m 755 runc.arm64 /usr/local/bin/runc
+
+# Containerd
+mkdir containerd
+tar -xvf containerd-2.1.0-beta.0-linux-arm64.tar.gz -C containerd
+cp containerd/bin/containerd containerd/bin/containerd-shim-runc-v2 containerd/bin/containerd-stress /bin/
+cp ../configs/containerd-config.toml /etc/containerd/config.toml
+
+# etcd
+mkdir etcd
+tar -xvf etcd-v3.6.0-rc.3-linux-arm64.tar.gz -C etcd
+cp etcd/*/etcd* /usr/local/bin/
+chmod 700 /var/lib/etcd
+cp ../certificates/ca.crt ../certificates/kube-api-server.key ../certificates/kube-api-server.crt /etc/etcd/
+
+# Kubernetes
+chmod +x kube-apiserver \
+    kube-controller-manager \
+    kube-scheduler kubectl \
+    kube-proxy kubelet
+
+cp kube-apiserver \
+    kube-controller-manager \
+    kube-scheduler kubectl \
+    kube-proxy kubelet \
+    /usr/local/bin/
+
+cd ..
---
+++ bash/02-kubeconfigs.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+cd certificates
+
+# kubelet
+kubectl config set-cluster krabernetes \
+    --certificate-authority=ca.crt \
+    --embed-certs=true \
+    --server=https://127.0.0.1:6443 \
+    --kubeconfig=vagrant.kubeconfig
+
+kubectl config set-credentials system:node:vagrant \
+    --client-certificate=vagrant.crt \
+    --client-key=vagrant.key \
+    --embed-certs=true \
+    --kubeconfig=vagrant.kubeconfig
+
+kubectl config set-context default \
+    --cluster=krabernetes \
+    --user=system:node:vagrant \
+    --kubeconfig=vagrant.kubeconfig
+
+kubectl config use-context default \
+    --kubeconfig=vagrant.kubeconfig
+
+# kube-proxy
+kubectl config set-cluster krabernetes \
+    --certificate-authority=ca.crt \
+    --embed-certs=true \
+    --server=https://127.0.0.1:6443 \
+    --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config set-credentials system:kube-proxy \
+    --client-certificate=kube-proxy.crt \
+    --client-key=kube-proxy.key \
+    --embed-certs=true \
+    --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config set-context default \
+    --cluster=krabernetes \
+    --user=system:kube-proxy \
+    --kubeconfig=kube-proxy.kubeconfig
+
+kubectl config use-context default \
+    --kubeconfig=kube-proxy.kubeconfig
+
+# kube-controller-manager
+kubectl config set-cluster krabernetes \
+    --certificate-authority=ca.crt \
+    --embed-certs=true \
+    --server=https://127.0.0.1:6443 \
+    --kubeconfig=kube-controller-manager.kubeconfig
+
+kubectl config set-credentials system:kube-controller-manager \
+    --client-certificate=kube-controller-manager.crt \
+    --client-key=kube-controller-manager.key \
+    --embed-certs=true \
+    --kubeconfig=kube-controller-manager.kubeconfig
+
+kubectl config set-context default \
+    --cluster=krabernetes \
+    --user=system:kube-controller-manager \
+    --kubeconfig=kube-controller-manager.kubeconfig
+
+kubectl config use-context default \
+    --kubeconfig=kube-controller-manager.kubeconfig
+
+# kube-scheduler
+kubectl config set-cluster krabernetes \
+    --certificate-authority=ca.crt \
+    --embed-certs=true \
+    --server=https://127.0.0.1:6443 \
+    --kubeconfig=kube-scheduler.kubeconfig
+
+kubectl config set-credentials system:kube-scheduler \
+    --client-certificate=kube-scheduler.crt \
+    --client-key=kube-scheduler.key \
+    --embed-certs=true \
+    --kubeconfig=kube-scheduler.kubeconfig
+
+kubectl config set-context default \
+    --cluster=krabernetes \
+    --user=system:kube-scheduler \
+    --kubeconfig=kube-scheduler.kubeconfig
+
+kubectl config use-context default \
+    --kubeconfig=kube-scheduler.kubeconfig
+
+# admin
+kubectl config set-cluster krabernetes \
+    --certificate-authority=ca.crt \
+    --embed-certs=true \
+    --server=https://127.0.0.1:6443 \
+    --kubeconfig=admin.kubeconfig
+
+kubectl config set-credentials admin \
+    --client-certificate=admin.crt \
+    --client-key=admin.key \
+    --embed-certs=true \
+    --kubeconfig=admin.kubeconfig
+
+kubectl config set-context default \
+    --cluster=krabernetes \
+    --user=admin \
+    --kubeconfig=admin.kubeconfig
+
+kubectl config use-context default \
+    --kubeconfig=admin.kubeconfig
+
+cd ..
